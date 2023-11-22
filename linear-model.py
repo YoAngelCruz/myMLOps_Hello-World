733efcacd5f93cd981484d70494efa15073833c5
angel <angelcruz.feb@gmail.com>
2023-11-22 07:44:58 -0600

    edit model

--- linear-model.py
+++ linear-model.py
@@ -1,37 +1,32 @@
+
 import numpy as np
 import os
+#import matplotlib.pyplot as plt
+
+# TensorFlow for calculate the weight 
 import tensorflow as tf
+ 
+print(tf.__version__)
 
-# Datos de ejemplo (reemplaza estos datos con tu propio conjunto de datos)
-L = np.arange(100, 600, 100)  # Número de líneas de código
-C = np.array([3, 5, 4, 6, 7])  # Nivel de complejidad
-Errores = np.array([10, 15, 12, 18, 20])  # Número real de errores
+X = np.arange(1, 11, 1)  
+np.random.shuffle(X)
+y = X * 8.91  
 
-train_end = int(0.6 * len(L))
-test_start = int(0.8 * len(L))
-
-L_train, C_train, Errores_train = L[:train_end], C[:train_end], Errores[:train_end]
-L_test, C_test, Errores_test = L[test_start:], C[test_start:], Errores[test_start:]
-L_val, C_val, Errores_val = L[train_end:test_start], C[train_end:test_start], Errores[train_end:test_start]
+train_end = int(0.6 * len(X))
+test_start = int(0.8 * len(X))
+X_train, y_train = X[:train_end], y[:train_end]
+X_test, y_test = X[test_start:], y[test_start:]
+X_val, y_val = X[train_end:test_start], y[train_end:test_start]
 
 tf.keras.backend.clear_session()
-
-# Definir el modelo para predecir errores o bugs
 linear_model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(units=1, input_shape=[2], name='Single')
+    tf.keras.layers.Dense(units=1, input_shape=[1], name='Single')
 ])
-
 linear_model.compile(optimizer=tf.keras.optimizers.SGD(), loss=tf.keras.losses.mean_squared_error)
 print(linear_model.summary())
 
-# Entrenar el modelo para predecir errores o bugs
-linear_model.fit(x=np.column_stack((L_train, C_train)), y=Errores_train,
-              validation_data=(np.column_stack((L_val, C_val)), Errores_val), epochs=20)
+linear_model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=20)
+print(linear_model.predict([[1.0], [2.0], [3.0], [4.0], [5.0]]).tolist())   
 
-# Predicción de errores o bugs para algunos valores
-predictions = linear_model.predict(np.column_stack(([0.0, 200, 310, 420, 520], [3, 5, 4, 6, 7]))).tolist()
-print("Predicciones:", predictions)
-
-# Guardar el modelo entrenado
 export_path = 'linear-model/1/'
 tf.saved_model.save(linear_model, os.path.join('./', export_path))
--- model_client.py
+++ model_client.py
@@ -2,25 +2,26 @@ import json
 import streamlit as st
 import requests
 
-SERVER_URL = 'https://bug-model-service-yoangelcruz.cloud.okteto.net/v1/models/bug-model:predict'
+SERVER_URL = 'https://linear-model-service-yoangelcruz.cloud.okteto.net/v1/models/linear-model:predict'
 
 def make_prediction(inputs):
-    predict_request = {'instances': inputs}
+   
+    predict_request = {'instances': [inputs]}
     response = requests.post(SERVER_URL, json=predict_request)
     response.raise_for_status()
     prediction = response.json()
     return prediction
 
 def main():
-    st.title('Predicción de Errores en Software')
+    st.title('Estimador del peso de un objeto basado en su masa')
 
-    code_size = st.number_input('Ingrese el número de líneas de código:', min_value=0, step=1)
-    complexity = st.number_input('Ingrese la complejidad del código:', min_value=0, step=1)
+   
+    code_size = st.number_input('Ingrese la masa del objeto:', min_value=0.0, step=1.0)
 
+   
     if st.button('Predecir'):
-        prediction = make_prediction([[code_size, complexity]])
-        predicted_errors = prediction['predictions'][0][0]
-        st.write(f'Número estimado de errores para un código de {code_size} líneas y complejidad {complexity}: {predicted_errors}')
+        prediction = make_prediction([code_size])
+        st.write(f'Peso del objeto basado en la masa de {code_size}: {prediction["predictions"][0][0]}')
 
 if __name__ == '__main__':
     main()
